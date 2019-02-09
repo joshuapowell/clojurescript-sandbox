7883ecc119107111319208cc8e3b64fa395f01a5
Joshua Powell <joshua@viable.io>
2019-02-09 13:04:56 -0500

    Added average function example; Added first pass at a Coordinate Conversion function

--- src/hello_human/core.cljs
+++ src/hello_human/core.cljs
@@ -1,3 +1,26 @@
 (ns hello_human.core)
 
 (println "Hello there, human")
+
+;;
+;; Average
+;;
+;; Usage:
+;;
+;; (require '[hello-human.core :as human] :reload)
+;; (human/average 20 13)
+;;
+;; : return (float) 16.5
+;;
+(defn average [a b]
+  (/ (+ a b) 2.0))
+
+;;
+;; Coordinate Conversion, Degree-Minute-Second to Decimal Degrees
+;;
+;; Equation:
+;;
+;; dd = d + (m/60) + (s/3600)
+;;
+(defn dmsToDd [d m s]
+  (+ d (/ m 60) (/ s 3600)))
