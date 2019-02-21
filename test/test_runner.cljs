4ed6db2dededf2c0387ddc270638e70a2047ad94
Joshua Powell <joshua@viable.io>
2019-02-21 13:57:17 -0500

    Revised gitignore, installed a new Clojure project using shadow-cljs

--- test/test_runner.cljs
+++
@@ -1,7 +0,0 @@
-(ns testcljs.test-runner
-  (:require [testcljs.core-test]
-            [doo.runner :refer-macros [doo-tests doo-all-tests]]))
-
-(enable-console-print!)
-
-(doo-tests 'testcljs.core-test)
